bbf9892c768285d4499d95a4cd85ded3b6113f3c
Albin Lang <alla1603@student.miun.se>
2020-07-02 21:36:51 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ParallaxDirective } from './parallax.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParallaxDirective
   ],
   imports: [
     BrowserModule
